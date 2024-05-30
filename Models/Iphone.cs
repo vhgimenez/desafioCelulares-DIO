08016c263f8fff53adbe09b732cc0b2d7c5c1873
vhgimenez <impressaogimenez@gmail.com>
2024-05-30 18:39:56 -0300

    finalizado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Cria uma instância da classe Nokia
+        Smartphone nokia = new Nokia("123456", "Modelo A", "111111111", 64);
+        
+        // Testando os métodos da classe Nokia
+        Console.WriteLine("Teste com Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+        Console.WriteLine($"Número: {nokia.Numero}, Modelo: {nokia.Modelo}, IMEI: {nokia.IMEI}, Memória: {nokia.Memoria}GB");
+        Console.WriteLine();
+
+        // Cria uma instância da classe Iphone
+        Smartphone iphone = new Iphone("654321", "Modelo X", "222222222", 128);
+        
+        // Testando os métodos da classe Iphone
+        Console.WriteLine("Teste com iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+        Console.WriteLine($"Número: {iphone.Numero}, Modelo: {iphone.Modelo}, IMEI: {iphone.IMEI}, Memória: {iphone.Memoria}GB");
+    }
+}
